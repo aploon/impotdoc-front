7706a86e745ffe49ab14999fe89fb50f1b0fa424
Arnaud <arnaudadjovi274@gmail.com>
2024-11-03 19:49:37 +0100

    Update dependencies for Algolia search functionality

--- src/components/pages/SearchPage.vue
+++ src/components/pages/SearchPage.vue
@@ -1,14 +1,24 @@
 <template>
 
     <SearchComponent />
-    
+
+    <ais-instant-search index-name="front_impotdoc_index_php" :search-client="searchClient">
+        <!-- Widgets -->
+    </ais-instant-search>
+
 </template>
 
 <script setup>
 import SearchComponent from '@/components/utils/Search.vue';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 
-onMounted(()=>{
+// ref
+const searchClient = ref(
+    algoliasearch('V1PX7KYJ9H', '37794dba24017d8b506a02633a075d62')
+)
+
+// hooks
+onMounted(() => {
 
     // handle search when click on input open modal
     document.getElementById('search_open_btn').addEventListener('click', function () {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { routes } from './routes.js'
+import InstantSearch from 'vue-instantsearch/vue3/es';
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -23,5 +24,6 @@ router.afterEach(() => {
 const app = createApp(App)
 
 app.use(router)
+app.use(InstantSearch)
 
 app.mount('#app')
